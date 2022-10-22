ccadecdab171c035aaaf40ad789b2bbca1534a4f
Rye Miller <rmiller@corra.com>
2022-10-22 13:30:08 -0600

    Update of aliases into one file.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,26 +1,88 @@
-#!/usr/bin/env bash
-# Aliases dotfile (.aliases)
 #
-# Various helpful and fun aliases for development.
+# TODO: write script description
 #
-# Version 0.1.0 [2020-12-27]
-# http://github.com/iods/dotfiles
-# Copyright (c) 2020, Rye Miller
+
 #
-# echo "Loaded [5] fifth in the sequence."
+# Changing and Navigating Directory, and `ls` Aliases
+#
+alias .='pwd'                    # print working directory
+alias ..='cd ..'                 # go up one directory
+alias cd..='cd ..'               # go up one directory
+alias ...='cd ../..'             # go up two directories
+alias 3.='cd ../../../'          # go up three directories
+alias 4.='cd ../../../../'       # go up four directories
+alias 5.='cd ../../../../../'    # go up five directories
+alias ~='cd "$HOME"'             # go to the home directory
+alias -- -='cd -'                # go back to previous directory
 
-# Some sudo work
-# http://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo
-alias sudo='sudo '
+alias path='echo "$PATH" | tr -s ":" "\n"'    # pretty print the $PATHs
+alias fpath='print -l "$FPATH"'  # pretty print the $FPATHs
 
-# Ubuntu OS
+alias tree='tree --dirsfirst'    # get description
 
-# Navigation
+if ls --color > /dev/null 2>&1; then
+  colorflag="--color"
+else
+  colorflag="-G"
+fi
 
-# Path work
-alias path="print -l $PATH"
-alias fpath="print -l $FPATH"
+alias ls='ls -hF ${colorflag}'   # classify files in colors
+alias ll='ls -ltr'               # just a long list
+alias la='ls -lA'                # list all but . and ..
+alias l='ls -CF'                 # get description
 
-# Wget/Curl
+alias df='df -h'                 # default to Human Readable Figures
+alias du='du -h'                 # default to Human Readable Figures
+
+#
+# Git Aliases
+#
+alias gs='git status'            # show status of current git branch
+alias gd='git diff HEAD'         # show whats changed, staged and unstaged
+
+
+#
+# Management
+#
+alias diskstat='watch -c -d -n 1 "sudo S_COLORS=always iostat -xmdzc -t 1 1"'
+alias diskuse='watch -c -d -n 1 "sudo S_COLORS=always sar -d 0"'
+alias httpdump='sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\"'
+alias iotop='sudo iotop -oa'
+alias mpstat='watch -c -d -n 1 "sudo S_COLORS=always mpstat -P ALL"'
+alias netuse='watch -c -d -n 1 "sudo S_COLORS=always sar -n DEV 0"'
+alias sniff='lsof -i 4tcp'
+alias tf='tail -F -n200'
+alias top='top -ocpu'
+alias vmstat='vmstat -Sm 1'
+
+alias grep='grep --color=auto '
+alias p='ps aux | grep -v ]$'
+alias pg='ps aux | head n1; ps aux | grep -i'
+alias sudo='sudo '  # http://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo
+
+alias sc-dreload='sudo systemctl daemon-reload'
+
+#
+# Misc Aliases
+#
+alias cp='cp -i'
+alias dots='cd "${HOME}"/.dotfiles'
 alias get="wget --no-clobber --page-requisites --html-extension --convert-links --no-host-directories"
+alias hosts='sudo vim /etc/hosts'
+alias mkdir='mkdir -p'
+alias mv='mv -i'
 alias pull="curl -O -L"
+alias q='logout'
+alias :q='logout'
+alias reload='source "${HOME}"/.bash_profile && echo "sourced ~/.bash_profile"'
+alias rm='rm -i'
+alias updot='printf "Updating dotfiles."'
+
+#
+# OSX Aliases
+#
+
+#
+# Ubuntu OS Aliases
+#
+alias open='xdg-open'
