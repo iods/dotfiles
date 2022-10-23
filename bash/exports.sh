15f38990db46a4a4f9adc62d0ea6c92597573589
Rye Miller <rmiller@corra.com>
2022-10-22 18:27:06 -0600

    Addition of exports support for .zshrc.

---
+++ bash/exports.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# TODO: write script description
+#
+
+
+export EDITOR='vim'                       # make vim the default editor
+
+export HIST_TIME_FORMAT='%d/%m/%y %T ';   # TODO: get description
+
+
+#
+# Additional PATH exports
+#
